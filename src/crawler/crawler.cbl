c9b293f404d5e3282bbd83f4b6e16464aaf979b2
Ernest Pazera <ernestpazera@gmail.com>
2022-12-29 05:23:14 -0600

    left door

--- src/crawler/crawler.cbl
+++ src/crawler/crawler.cbl
@@ -14,6 +14,9 @@ DATA DIVISION.
            02 AheadDoorImage.
               03 AheadDoorImageRows OCCURS 10 TIMES.
                  04 AheadDoorImagePixels PIC X OCCURS 10 TIMES.
+           02 LeftDoorImage.
+              03 LeftDoorImageRows OCCURS 16 TIMES.
+                 04 LeftDoorImagePixels PIC X OCCURS 6 TIMES.
        01 ScratchPad.
           02 RenderRow PIC 99.
              88 NoMoreRows VALUES 25 THRU 99.
@@ -32,11 +35,18 @@ STOP RUN.
 DrawRoom.
        PERFORM DrawRoomFrame
        PERFORM DrawAheadDoor
+       PERFORM DrawLeftDoor
 EXIT.
 
 DrawAheadDoor.
        PERFORM VARYING SourceRow FROM 1 BY 1 UNTIL SourceRow IS GREATER THAN 10 AFTER SourceColumn FROM 1 BY 1 UNTIL SourceColumn IS GREATER THAN 10
            MOVE AheadDoorImagePixels(SourceRow, SourceColumn) TO FramePixels(SourceRow + 9, SourceColumn + 23)
+       END-PERFORM
+EXIT.
+
+DrawLeftDoor.
+       PERFORM VARYING SourceRow FROM 1 BY 1 UNTIL SourceRow IS GREATER THAN 16 AFTER SourceColumn FROM 1 BY 1 UNTIL SourceColumn IS GREATER THAN 6
+           MOVE LeftDoorImagePixels(SourceRow, SourceColumn) TO FramePixels(SourceRow + 7, SourceColumn + 3)
        END-PERFORM
 EXIT.
 
@@ -55,6 +65,7 @@ EXIT.
 InitializeImages.
        PERFORM InitializeRoomImage
        PERFORM InitializeAheadDoorImage
+       PERFORM InitializeLeftDoorImage
 EXIT.
 
 InitializeRoomImage.
@@ -94,4 +105,23 @@ InitializeAheadDoorImage.
        MOVE "|        |" TO AheadDoorImageRows(7)
        MOVE "|        |" TO AheadDoorImageRows(8)
        MOVE "+        +" TO AheadDoorImageRows(9)
+EXIT.
+
+InitializeLeftDoorImage.
+       MOVE "+\_   " TO LeftDoorImageRows(1)
+       MOVE "|  \_ " TO LeftDoorImageRows(2)
+       MOVE "|    +" TO LeftDoorImageRows(3)
+       MOVE "|    |" TO LeftDoorImageRows(4)
+       MOVE "|    |" TO LeftDoorImageRows(5)
+       MOVE "|    |" TO LeftDoorImageRows(6)
+       MOVE "|    |" TO LeftDoorImageRows(7)
+       MOVE "|    |" TO LeftDoorImageRows(8)
+       MOVE "|    |" TO LeftDoorImageRows(9)
+       MOVE "|    |" TO LeftDoorImageRows(10)
+       MOVE "|    |" TO LeftDoorImageRows(11)
+       MOVE "|    |" TO LeftDoorImageRows(12)
+       MOVE "|    +" TO LeftDoorImageRows(13)
+       MOVE "|     " TO LeftDoorImageRows(14)
+       MOVE "|     " TO LeftDoorImageRows(15)
+       MOVE "+     " TO LeftDoorImageRows(16)
 EXIT.
