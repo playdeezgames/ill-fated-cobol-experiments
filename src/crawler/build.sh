e439aef7233bcf1885663ac74392c7ff6a8c820f
Ernest Pazera <ernestpazera@gmail.com>
2022-12-29 04:57:14 -0600

    some crawler stuff

---
+++ src/crawler/build.sh
@@ -0,0 +1,1 @@
+cobc -x crawler.cbl rng.cbl
---
+++ src/crawler/crawler.cbl
@@ -0,0 +1,92 @@
+       >>SOURCE FORMAT FREE
+IDENTIFICATION DIVISION.
+PROGRAM-ID. CRAWLER.
+
+DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 FrameData.
+           02 FrameRows OCCURS 24 TIMES.
+               03 FrameColumns OCCURS 56 TIMES.
+                  04 FramePixels PIC X.
+       01 ImageData.
+           02 RoomImage.
+              03 RoomImageRows PIC X(56) OCCURS 24 TIMES.
+           02 AheadDoorImage.
+              02 AheadDoorImageRows PIC X(10) OCCURS 10 TIMES.
+       01 ScratchPad.
+          02 RenderRow PIC 99.
+             88 NoMoreRows VALUES 25 THRU 99.
+          02 RenderColumn PIC 99.
+          02 FramePixel PIC X.
+
+PROCEDURE DIVISION.
+       CALL "RNG"
+       PERFORM InitializeImages
+       PERFORM DrawRoom
+       PERFORM PresentFrame
+STOP RUN.
+
+DrawRoom.
+       PERFORM DrawRoomFrame
+       PERFORM DrawAheadDoor
+EXIT.
+
+DrawAheadDoor.
+       
+EXIT.
+
+DrawRoomFrame.
+       MOVE RoomImage TO FrameData
+EXIT.
+
+PresentFrame.
+       PERFORM PresentFrameRow VARYING RenderRow FROM 1 BY 1 UNTIL NoMoreRows
+EXIT.
+
+PresentFrameRow.
+       DISPLAY FrameRows(RenderRow)
+EXIT.
+
+InitializeImages.
+       PERFORM InitializeRoomImage
+       PERFORM InitializeAheadDoorImage
+EXIT.
+
+InitializeRoomImage.
+       MOVE "\_                                                    _/" TO RoomImageRows(1)
+       MOVE "  \_                                                _/  " TO RoomImageRows(2)
+       MOVE "    \_                                            _/    " TO RoomImageRows(3)
+       MOVE "      \_                                        _/      " TO RoomImageRows(4)
+       MOVE "        \_                                    _/        " TO RoomImageRows(5)
+       MOVE "          \_                                _/          " TO RoomImageRows(6)
+       MOVE "            +------------------------------+            " TO RoomImageRows(7)
+       MOVE "            |                              |            " TO RoomImageRows(8)
+       MOVE "            |                              |            " TO RoomImageRows(9)
+       MOVE "            |                              |            " TO RoomImageRows(10)
+       MOVE "            |                              |            " TO RoomImageRows(11)
+       MOVE "            |                              |            " TO RoomImageRows(12)
+       MOVE "            |                              |            " TO RoomImageRows(13)
+       MOVE "            |                              |            " TO RoomImageRows(14)
+       MOVE "            |                              |            " TO RoomImageRows(15)
+       MOVE "            |                              |            " TO RoomImageRows(16)
+       MOVE "            |                              |            " TO RoomImageRows(17)
+       MOVE "            +------------------------------+            " TO RoomImageRows(18)
+       MOVE "          _/                                \_          " TO RoomImageRows(19)
+       MOVE "        _/                                    \_        " TO RoomImageRows(20)
+       MOVE "      _/                                        \_      " TO RoomImageRows(21)
+       MOVE "    _/                                            \_    " TO RoomImageRows(22)
+       MOVE "  _/                                                \_  " TO RoomImageRows(23)
+       MOVE "_/                                                    \_" TO RoomImageRows(24)
+EXIT.
+
+InitializeAheadDoorImage.
+       MOVE "+--------+" TO AheadDoorImageRows(1)
+       MOVE "|        |" TO AheadDoorImageRows(2)
+       MOVE "|        |" TO AheadDoorImageRows(3)
+       MOVE "|        |" TO AheadDoorImageRows(4)
+       MOVE "|        |" TO AheadDoorImageRows(5)
+       MOVE "|        |" TO AheadDoorImageRows(6)
+       MOVE "|        |" TO AheadDoorImageRows(7)
+       MOVE "|        |" TO AheadDoorImageRows(8)
+       MOVE "+        +" TO AheadDoorImageRows(9)
+EXIT.
