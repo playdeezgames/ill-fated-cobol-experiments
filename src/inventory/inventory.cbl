498351da2be5ec89c958bd12c1f355bd151c3bf7
Ernest Pazera <ernestpazera@gmail.com>
2022-12-30 05:22:48 -0600

    adding item data

--- src/inventory/inventory.cbl
+++ src/inventory/inventory.cbl
@@ -12,31 +12,40 @@ DATA DIVISION.
                    88 IsChamber VALUE "C".
              03 LocationId PIC 999 VALUE 1. 
                 88 InvalidId VALUES 101 THRU 999.
+          02 ItemData.
+             03 Items OCCURS 100 TIMES.
+                04 ItemTypes PIC 99 VALUE ZEROS.
+                   88 NotAnItem VALUE 0.
+                   88 Food VALUE 1.
+                   88 Potion VALUE 2.
+                04 ItemStatus PIC 999 VALUE ZEROS.
+             03 ItemId PIC 999 VALUE 1.
+                88 InvalidId VALUES 101 THRU 999.
        01 ScratchPad.
-          02 CurrentLocationId PIC 999.
+          02 LocationId PIC 999.
           02 Dummy PIC 999.
 PROCEDURE DIVISION.
        CALL "RNGSEED"
        PERFORM InitializeLocations
-       PERFORM DisplayLocationType VARYING CurrentLocationId FROM 1 BY 1 UNTIL CurrentLocationId IS GREATER THAN 100
+       PERFORM DisplayLocationType VARYING LocationId OF ScratchPad FROM 1 BY 1 UNTIL LocationId OF ScratchPad IS GREATER THAN 100
 STOP RUN.
 
 DisplayLocationType.
-       DISPLAY CurrentLocationId " = " LocationTypes(CurrentLocationId)
+       DISPLAY LocationId OF ScratchPad " = " LocationTypes(LocationId OF ScratchPad)
 EXIT.
 
 InitializeLocations.
-       MOVE 1 TO LocationId
-       PERFORM InitializeLocation UNTIL InvalidId
+       MOVE 1 TO LocationId OF LocationData
+       PERFORM InitializeLocation UNTIL InvalidId OF LocationData
 EXIT.
 
 InitializeLocation.
        COMPUTE Dummy = FUNCTION RANDOM() * 2
        EVALUATE Dummy
         WHEN 0
-           SET IsPassageway(LocationId) TO TRUE
+           SET IsPassageway(LocationId OF LocationData) TO TRUE
         WHEN 1
-           SET IsChamber(LocationId) TO TRUE
+           SET IsChamber(LocationId OF LocationData) TO TRUE
        END-EVALUATE
-       ADD 1 TO LocationId
+       ADD 1 TO LocationId OF LocationData
 EXIT.
