4d6d8f9ca3ec85e6e225445e6d1de1d6e57e6bd4
Ernest Pazera <ernestpazera@gmail.com>
2022-12-31 09:56:57 -0600

    sandbox mods

--- src/sandbox/sandbox.cbl
+++ src/sandbox/sandbox.cbl
@@ -9,9 +9,14 @@ DATA DIVISION.
              88 InvalidX VALUES 11 THRU 99.
           02 YValue PIC 99.
              88 InvalidY VALUES 11 THRU 99.
+          02 FlagValue PIC X.
+             88 FlagIsSet VALUE "Y".
+             88 FlagIsClear VALUE "N".
 
 PROCEDURE DIVISION.
+       SET FlagIsClear TO TRUE
        PERFORM VARYING YValue FROM 1 BY 1 UNTIL InvalidY AFTER XValue FROM 1 BY 1 UNTIL InvalidX
-           DISPLAY "(" XValue "," YValue")"
+           DISPLAY ScratchPad
        END-PERFORM
+       DISPLAY ScratchPad
 STOP RUN.
